9d6afcf85881695d9182f893d12ddae31c4ee12a
Se AKi <seaki@seaki.sastudio.jp>
2018-11-22 11:48:51 +0900

    Remove testField

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -6,7 +6,6 @@ class GraphqlController < ApplicationController
     context = {
       # Query context goes here, for example:
       # current_user: current_user,
-      time: Time.now
     }
     result = RubicureGraphqlSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,14 +5,6 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      Rails.logger.info context[:time]
-      "Hello World!"
-    end
-
     field :human_name, String, null: false do
       argument :girl_name, String, required: true
     end
