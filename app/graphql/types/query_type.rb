1a7e6d85800a7aa682486bd3bcbc339cf6355f4b
Se AKi <seaki@seaki.sastudio.jp>
2018-11-22 11:49:16 +0900

    Remove humanName

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,13 +5,6 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :human_name, String, null: false do
-      argument :girl_name, String, required: true
-    end
-    def human_name(girl_name:)
-      "#{Rubicure::Girl.find(girl_name.intern).human_name}"
-    end
-
     field :girl, Types::GirlType, null: false do
       argument :girl_name, String, required: true
     end
