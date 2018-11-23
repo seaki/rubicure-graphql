3cddadec73d1caf9cf9c4039b81e427329efa5ff
Se AKi <seaki@seaki.sastudio.jp>
2018-11-23 15:31:46 +0900

    Girl property returns bare object

--- app/graphql/types/girl_type.rb
+++ app/graphql/types/girl_type.rb
@@ -13,29 +13,30 @@ module Types
     field :attack_messages, [String], null: true, description: "Precure's message when attack"
     field :transform_calls, [String], null: true, description: "Precure's transformation call"
     field :full_name, String, null: false, description: "Precure's name before transformation(full name or human name)"
+    field :transform_styles, [String], null: true
 
     def girl_name
-      "#{object.girl_name}"
+      object.girl_name
     end
 
     def human_name
-      "#{object.human_name}"
+      object.human_name
     end
 
     def human_full_name
-      "#{object.human_full_name}"
+      object.human_full_name
     end
 
     def precure_name
-      "#{object.precure_name}"
+      object.precure_name
     end
 
     def cast_name
-      "#{object.cast_name}"
+      object.cast_name
     end
 
     def color
-      "#{object.color}"
+      object.color
     end
 
     def created_date
@@ -43,30 +44,31 @@ module Types
     end
 
     def birthday
-      "#{object.birthday}"
+      object.birthday
     end
 
     def transform_message
-      "#{object.transform_message}"
+      object.transform_message # まほプリのときは定義されていないのでnilが帰る、transform_stylesにスタイル指定しろとするか
     end
 
     def extra_names
       return object.extra_names if object.extra_names.is_a?(Array)
-      []
     end
 
     def attack_messages
       return object.attack_messages if object.attack_messages.is_a?(Array)
-      []
     end
 
     def transform_calls
       return object.transform_calls if object.transform_calls.is_a?(Array)
-      []
     end
 
     def full_name
-      "#{object.full_name}"
+      object.full_name
+    end
+
+    def transform_styles
+      object.transform_styles if object.method_defined?(:transform_styles)
     end
   end
 end
