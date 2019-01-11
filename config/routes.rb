223d10bd4d913b5dcf709ccd5243c3434664bb3e
seaki <se-aki-personal@sastudio.jp>
2019-01-11 21:19:51 +0900

    Change root page : redirect to GraphiQL

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: redirect('/graphiql')
   # if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   # end
