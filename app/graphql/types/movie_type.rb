1e60279c4cd1ae844e22fbe4758e7c1b3611dca6
seaki <se-aki-personal@sastudio.jp>
2019-01-12 23:26:05 +0900

    Add girls API in Movie API

--- app/graphql/types/movie_type.rb
+++ app/graphql/types/movie_type.rb
@@ -3,8 +3,12 @@ module Types
     field :title, String, null: false, description: "Movie's title"
     field :started_date, String, null: false, description: "Movie's opening day"
     field :name, String, null: false, description: "Movie's symbol"
+    field :girls, [Types::GirlType], null: false, description: "Girls apper in the movie"
     def name
       Rubicure::Movie.config.key(object)
+    end
+    def girls
+      Precure.all_stars(Rubicure::Movie.config.key(object))
     end
   end
 end
