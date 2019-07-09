5def2b24b9b7a4d07cb20f1ed6fa97e689f1e936
seaki <se-aki-personal@sastudio.jp>
2019-07-09 20:34:03 +0900

    Disable GraphiQL unless env.development? or ENABLE_GRAPHIQL is unset

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get '/', to: redirect('/graphiql')
-  # if Rails.env.development?
+  if (Rails.env.development? || ENV["ENABLE_GRAPHIQL"])
+    get '/', to: redirect('/graphiql')
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  # end
+  end
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
