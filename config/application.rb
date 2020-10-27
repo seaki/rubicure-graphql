7c89970ea5566feed563b0427b6594c977721f4b
Se AKi <seaki@sastudio.jp>
2020-10-27 21:53:51 +0900

    Bump version

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,6 @@ module RubicureGraphql
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.x.version = "1.3.4.0"
+    config.x.version = "1.3.5.0"
   end
 end
