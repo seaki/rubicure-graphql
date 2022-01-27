94416a6d6e5e74a78d4c63df38389641d816dc9a
Se AKi <seaki@sastudio.jp>
2022-01-28 00:09:03 +0900

    Impl. Randomized Tropical-Rouge! Precure's #transform! (a.k.a. #precure_tropical_change!) transformation message

--- app/graphql/types/transform_message_type.rb
+++ app/graphql/types/transform_message_type.rb
@@ -6,7 +6,12 @@ module Types
       object.first
     end
     def transform_message
-      object.last[:transform_message]
+      if object.last[:random_transform_words] && !object.last[:random_transform_words].empty?
+        random_transform_word = object.last[:random_transform_words].sample
+        object.last[:transform_message].gsub("${random_transform_word}", random_transform_word)
+      else
+        object.last[:transform_message]
+      end
     end
   end
 end
